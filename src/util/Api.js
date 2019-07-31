e25953c67079cddc66dc0bc87124c8077e3e26de
Dekoy <ayodeji.od@gmail.com>
2019-07-31 14:18:50 +0100

    vendors page

--- src/util/Api.js
+++ src/util/Api.js
@@ -4,8 +4,9 @@ import axios from 'axios';
 // import { get } from "lodash";
 
 export default axios.create({
+  baseURL:  `${process.env.REACT_APP_BACKEND_URL}`,
   // baseURL: `https://motuspro2.test/api/`,
-  baseURL: `https://api.motus.pro/api/`,
+  // baseURL: `https://api.motus.pro/api/`,
   headers: {
     'Content-Type': 'application/json',
   }
